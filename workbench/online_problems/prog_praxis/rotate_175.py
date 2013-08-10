fbfb9f3b88bacc7abeeed5519d16540d924aaad4
genos <graham.enos@gmail.com>
2013-08-09 20:18:53 -0400

    moved online_problems to its own repo

--- workbench/online_problems/prog_praxis/rotate_175.py
+++
@@ -1,5 +0,0 @@
-# Python answer to "Rotate an Array", 10/12/2010
-
-def rotate(array, nelts):
-    n = nelts % len(array)
-    return array[n:] + array[:n]
--- workbench/online_problems/rosalind/skeleton.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-if __name__ == "__main__":
-    with open("data/NAME") as f:
-        pass  # YOUR CODE HERE
