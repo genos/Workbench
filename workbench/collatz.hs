9ee31e59b57d86ae349fceb0ad68bf5a04d6bb23
genos <graham@quantifind.com>
2017-04-05 13:10:51 -0400

    tidy

--- workbench/collatz.hs
+++ workbench/collatz.hs
@@ -1,23 +1,14 @@
-c
-  :: (Integral a)
-  => a -> a
-c n
-  | even n = n `div` 2
-  | otherwise = 3 * n + 1
+c :: (Integral a) => a -> a
+c n | even n = n `div` 2
+    | otherwise = 3 * n + 1
 
-cs
-  :: (Integral a)
-  => a -> [a]
+cs :: (Integral a) => a -> [a]
 cs = iterate c
 
-collatz
-  :: (Integral a)
-  => a -> [a]
+collatz :: (Integral a) => a -> [a]
 collatz n = takeWhile (/= 1) (cs n) ++ [1]
 
-output
-  :: (Integral a)
-  => a -> IO ()
+output :: (Integral a) => a -> IO ()
 output n = putStrLn $ show n ++ ":\t" ++ show (collatz n)
 
 main :: IO ()
