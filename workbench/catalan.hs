b1d7dcdcfa58bbfbe3da5c76f46017a99f4e0de4
genos <graham@quantifind.com>
2017-06-06 15:45:52 -0400

    Haskell code tweaks

--- workbench/catalan.hs
+++ workbench/catalan.hs
@@ -1,7 +1,6 @@
 {- Catalan Numbers in Haskell, with some help from
  - http://rosettacode.org/wiki/Catalan_numbers#Haskell
  -}
-import Data.List       (subsequences)
 import Test.QuickCheck (quickCheck)
 
 -- helpers
@@ -9,10 +8,10 @@ binom :: Integer -> Integer -> Integer
 binom n k = product [k + 1 .. n] `div` product [1 .. n - k]
 
 factorial :: Integer -> Integer
-factorial n = product [2 .. n]
+factorial = product . enumFromTo 2
 
-pairs :: [a] -> [[a]]
-pairs = filter ((2 ==) . length) . subsequences
+pairs :: [a] -> [(a, a)]
+pairs xs = [(x, y) | x <- xs, y <- xs]
 
 -- Catalan definitions
 cat0 :: [Integer]
@@ -20,39 +19,35 @@ cat0 = map (\n -> product [n + 2 .. 2 * n] `div` product [2 .. n]) [0 ..]
 
 cat1 :: [Integer]
 cat1 = 1 : map c [1 ..]
-  where
-    c n = sum $ zipWith (*) (reverse (take n cat1)) cat1
+  where c n = sum $ zipWith (*) (reverse (take n cat1)) cat1
 
 cat2 :: [Integer]
 cat2 = scanl (\c n -> c * 2 * (2 * n - 1) `div` (n + 1)) 1 [1 ..]
 
 cat3 :: [Integer]
 cat3 = map c [0 ..]
-  where
-    c n
-      | n <= 0 = 1
+ where
+  c n | n <= 0    = 1
       | otherwise = binom (2 * n) n - binom (2 * n) (n + 1)
 
 cat4 :: [Integer]
 cat4 = map c [0 ..]
-  where
-    c n = sum (map (\k -> binom n k ^ 2) [0 .. n]) `div` (n + 1)
+  where c n = sum (map (\k -> binom n k ^ 2) [0 .. n]) `div` (n + 1)
 
 cat5 :: [Integer]
 cat5 = map (\n -> binom (2 * n) n `div` (n + 1)) [0 ..]
 
 cat6 :: [Integer]
 cat6 = map c [0 ..]
-  where
-    c n = factorial (2 * n) `div` (factorial (n + 1) * factorial n)
+  where c n = factorial (2 * n) `div` (factorial (n + 1) * factorial n)
 
 -- Check that these look the same
 prop_equal :: Int -> Bool
 prop_equal n = all p $ pairs cats
-  where
-    cats = [cat0, cat1, cat2, cat3, cat4, cat5, cat6]
-    p [xs, ys] = xs !! n' == ys !! n'
-    n' = n `mod` 1000
+ where
+  cats = [cat0, cat1, cat2, cat3, cat4, cat5, cat6]
+  p (xs, ys) = xs !! n' == ys !! n'
+  n' = n `mod` 1000
 
 main :: IO ()
 main = quickCheck prop_equal
