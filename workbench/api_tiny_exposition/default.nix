728f6c70f80350eb0ed5b2179a156af8a3743438
genos <graham@quantifind.com>
2020-08-17 14:32:29 -0400

    rename

--- workbench/api_tiny_exposition/default.nix
+++ workbench/api_tiny_exposition/default.nix
@@ -3,7 +3,7 @@ let
     (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz") { };
   py = pkgs.python38Full.withPackages (p: [ p.flask ]);
 in pkgs.stdenv.mkDerivation {
-  name = "tiny_api_demo";
+  name = "api_tiny_exposition";
   buildInputs = [ py ];
   shellHooks = "export FLASK_APP=app.py";
 }
