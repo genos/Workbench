c447865be3db3b544b2b33c84a004b35a54a987b
genos <graham@quantifind.com>
2020-08-17 13:58:44 -0400

    more housecleaning

--- workbench/swig_bottle/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-rm -f my_pair_wrap.cc;
-rm -f my_pair.py*;
-rm -f *.o;
-rm -f *.so;
--- workbench/take.scm
+++
@@ -1,5 +0,0 @@
-(define take
-  (lambda (n xs)
-    (if (or (eq? xs '()) (<= n 0))
-        '()
-        (cons (car xs) (take (cdr xs) (- n 1))))))
