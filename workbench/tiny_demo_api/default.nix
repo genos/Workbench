4300f5325949a997ef6a6706ecece4ddeeea7066
genos <graham@quantifind.com>
2020-08-20 11:09:14 -0400

    moving out to its own repo

--- workbench/tiny_demo_api/default.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import
-    (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz") { };
-  py = pkgs.python38Full.withPackages (p: [ p.flask ]);
-in pkgs.stdenv.mkDerivation {
-  name = "tiny_demo_api";
-  buildInputs = [ py ];
-  shellHooks = "export FLASK_APP=app.py";
-}
