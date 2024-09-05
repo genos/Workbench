3fda5c1ab8b83fb52828767b0ebd8c7d483e26b1
Graham Enos <graham@strangeworks.com>
2024-09-05 12:25:10 -0400

    use std::sync::LazyLock instead of once_cell::sync::Lazy

--- quantum-interpreter/src/matrices.rs
+++ quantum-interpreter/src/matrices.rs
@@ -1,16 +1,15 @@
 use ndarray::{array, linalg::kron, Array1, Array2};
 use num_complex::Complex64;
-use once_cell::sync::Lazy;
-use std::sync::Arc;
+use std::sync::{Arc, LazyLock};
 
 pub type Matrix = Array2<Complex64>;
 pub type Vector = Array1<Complex64>;
 
-static _1: Lazy<Complex64> = Lazy::new(|| Complex64::from(1.0));
-static _0: Lazy<Complex64> = Lazy::new(|| Complex64::from(0.0));
+static _1: LazyLock<Complex64> = LazyLock::new(|| Complex64::from(1.0));
+static _0: LazyLock<Complex64> = LazyLock::new(|| Complex64::from(0.0));
 
-pub static I: Lazy<Arc<Matrix>> = Lazy::new(|| Arc::new(Matrix::eye(2)));
-pub static SWAP: Lazy<Arc<Matrix>> = Lazy::new(|| {
+pub static I: LazyLock<Arc<Matrix>> = LazyLock::new(|| Arc::new(Matrix::eye(2)));
+pub static SWAP: LazyLock<Arc<Matrix>> = LazyLock::new(|| {
     Arc::new(array![
         [*_1, *_0, *_0, *_0],
         [*_0, *_0, *_1, *_0],
@@ -18,11 +17,11 @@ pub static SWAP: Lazy<Arc<Matrix>> = Lazy::new(|| {
         [*_0, *_0, *_0, *_1]
     ])
 });
-pub static H: Lazy<Arc<Matrix>> = Lazy::new(|| {
+pub static H: LazyLock<Arc<Matrix>> = LazyLock::new(|| {
     let s = Complex64::from(std::f64::consts::FRAC_1_SQRT_2);
     Arc::new(array![[s, s], [s, -s]])
 });
-pub static CNOT: Lazy<Arc<Matrix>> = Lazy::new(|| {
+pub static CNOT: LazyLock<Arc<Matrix>> = LazyLock::new(|| {
     Arc::new(array![
         [*_1, *_0, *_0, *_0],
         [*_0, *_1, *_0, *_0],
